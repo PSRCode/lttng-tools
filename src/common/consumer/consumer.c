14daaf16e5b153cb60fe70d034fb616860798be0
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2017-07-25 16:26:25 -0400

    FOR TESTING PURPOSE ONLY Cleanup: remove dead assignment
    Both calling sites do not use the return value and errors are already
    managed inside the called function.
    
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/consumer/consumer.c
+++ src/common/consumer/consumer.c
@@ -3356,7 +3356,7 @@ error:
  * This will create a relayd socket pair and add it to the relayd hash table.
  * The caller MUST acquire a RCU read side lock before calling it.
  */
-int consumer_add_relayd_socket(uint64_t net_seq_idx, int sock_type,
+ void consumer_add_relayd_socket(uint64_t net_seq_idx, int sock_type,
 		struct lttng_consumer_local_data *ctx, int sock,
 		struct pollfd *consumer_sockpoll,
 		struct lttcomm_relayd_sock *relayd_sock, uint64_t sessiond_id,
@@ -3517,7 +3517,7 @@ int consumer_add_relayd_socket(uint64_t net_seq_idx, int sock_type,
 	add_relayd(relayd);
 
 	/* All good! */
-	return 0;
+	return;
 
 error:
 	if (consumer_send_status_msg(sock, ret_code) < 0) {
@@ -3535,8 +3535,6 @@ error_nosignal:
 	if (relayd_created) {
 		free(relayd);
 	}
-
-	return ret;
 }
 
 /*
