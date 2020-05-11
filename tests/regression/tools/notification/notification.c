98d06660b0a79e574c9d4b330a3636f9ef30399a
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2020-05-11 12:22:02 -0400

    test: lifetime management of the action object
    The action object ownership is not transfered on create_trigger hence we
    need to carry the action object until we stop using the trigger object.
    
    Once the trigger is destroyed we can then destroy the action.
    
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
    Change-Id: I280258f1657c6a7c9635bd21e08e176cf7d4d068
    Depends-on: lttng-ust: I5a800fc92e588c2a6a0e26282b0ad5f31c044479

--- tests/regression/tools/notification/notification.c
+++ tests/regression/tools/notification/notification.c
@@ -864,6 +864,7 @@ static void create_tracepoint_event_rule_trigger(const char *event_pattern,
 		const char **exclusions,
 		enum lttng_domain_type domain_type,
 		struct lttng_condition **condition,
+		struct lttng_action **action,
 		struct lttng_trigger **trigger)
 {
 	enum lttng_event_rule_status event_rule_status;
@@ -936,6 +937,7 @@ static void create_tracepoint_event_rule_trigger(const char *event_pattern,
 	lttng_event_rule_destroy(event_rule);
 
 	*condition = tmp_condition;
+	*action = tmp_action;
 	*trigger = tmp_trigger;
 
 	return;
@@ -1004,7 +1006,7 @@ static void test_tracepoint_event_rule_notification(
 	}
 
 	create_tracepoint_event_rule_trigger(pattern, trigger_name, NULL, 0,
-			NULL, domain_type, &condition, &trigger);
+			NULL, domain_type, &condition, &action, &trigger);
 
 	notification_channel = lttng_notification_channel_create(
 			lttng_session_daemon_notification_endpoint);
@@ -1043,6 +1045,7 @@ static void test_tracepoint_event_rule_notification_filter(
 	enum lttng_notification_channel_status nc_status;
 
 	struct lttng_condition *ctrl_condition = NULL, *condition = NULL;
+	struct lttng_action *ctrl_action = NULL, *action = NULL;
 	struct lttng_notification_channel *notification_channel = NULL;
 	struct lttng_trigger *ctrl_trigger = NULL, *trigger = NULL;
 	const char *ctrl_trigger_name = "control_trigger";
@@ -1061,7 +1064,8 @@ static void test_tracepoint_event_rule_notification_filter(
 	ok(notification_channel, "Notification channel object creation");
 
 	create_tracepoint_event_rule_trigger(pattern, ctrl_trigger_name, NULL,
-			0, NULL, domain_type, &ctrl_condition, &ctrl_trigger);
+			0, NULL, domain_type, &ctrl_condition,
+			&ctrl_action, &ctrl_trigger);
 
 	nc_status = lttng_notification_channel_subscribe(
 			notification_channel, ctrl_condition);
@@ -1073,8 +1077,8 @@ static void test_tracepoint_event_rule_notification_filter(
 	 * `intfield` is even.
 	 */
 	create_tracepoint_event_rule_trigger(pattern, trigger_name,
-			"(intfield & 1) == 0", 0, NULL, domain_type, &condition,
-			&trigger);
+			"(intfield & 1) == 0", 0, NULL, domain_type,
+			&condition, &action, &trigger);
 
 	nc_status = lttng_notification_channel_subscribe(
 			notification_channel, condition);
@@ -1115,6 +1119,8 @@ static void test_tracepoint_event_rule_notification_filter(
 	lttng_trigger_destroy(ctrl_trigger);
 	lttng_condition_destroy(condition);
 	lttng_condition_destroy(ctrl_condition);
+	lttng_action_destroy(action);
+	lttng_action_destroy(ctrl_action);
 	return;
 }
 
@@ -1123,6 +1129,7 @@ static void test_tracepoint_event_rule_notification_exclusion(
 {
 	enum lttng_notification_channel_status nc_status;
 	struct lttng_condition *ctrl_condition = NULL, *condition = NULL;
+	struct lttng_action *ctrl_action = NULL, *action = NULL;
 	struct lttng_notification_channel *notification_channel = NULL;
 	struct lttng_trigger *ctrl_trigger = NULL, *trigger = NULL;
 	const char *ctrl_trigger_name = "control_exclusion_trigger";
@@ -1138,7 +1145,8 @@ static void test_tracepoint_event_rule_notification_exclusion(
 	ok(notification_channel, "Notification channel object creation");
 
 	create_tracepoint_event_rule_trigger(pattern, ctrl_trigger_name, NULL,
-			0, NULL, domain_type, &ctrl_condition, &ctrl_trigger);
+			0, NULL, domain_type, &ctrl_condition, &ctrl_action,
+			&ctrl_trigger);
 
 	nc_status = lttng_notification_channel_subscribe(
 			notification_channel, ctrl_condition);
@@ -1146,7 +1154,7 @@ static void test_tracepoint_event_rule_notification_exclusion(
 			"Subscribe to tracepoint event rule condition");
 
 	create_tracepoint_event_rule_trigger(pattern, trigger_name, NULL, 4,
-			exclusions, domain_type, &condition, &trigger);
+			exclusions, domain_type, &condition, &action, &trigger);
 
 	nc_status = lttng_notification_channel_subscribe(
 			notification_channel, condition);
@@ -1191,6 +1199,8 @@ static void test_tracepoint_event_rule_notification_exclusion(
 	lttng_trigger_destroy(ctrl_trigger);
 	lttng_condition_destroy(condition);
 	lttng_condition_destroy(ctrl_condition);
+	lttng_action_destroy(action);
+	lttng_action_destroy(ctrl_action);
 	return;
 }
 
