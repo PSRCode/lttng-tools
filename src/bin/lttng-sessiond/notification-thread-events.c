5a1f188389fc074f75072fd7b1647a0c8afa0514
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2020-05-13 19:02:43 -0400

    Support capture for kernel tracer
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
    Change-Id: I26167d259daa8e356a1bb03831ccf6e570b8e305
    Depends-on: lttng-ust: I8423c510bf6af2f9bf85256e8d6f931d36f7054b

--- src/bin/lttng-sessiond/notification-thread-events.c
+++ src/bin/lttng-sessiond/notification-thread-events.c
@@ -4212,7 +4212,7 @@ static struct lttng_event_notifier_notification *receive_notification(
 		break;
 	case LTTNG_DOMAIN_KERNEL:
 		token = kernel_notification.token;
-		capture_buffer_size = 0;
+		capture_buffer_size = kernel_notification.capture_buf_size;
 		break;
 	default:
 		abort();
