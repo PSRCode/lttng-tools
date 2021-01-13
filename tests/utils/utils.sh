PSRCode/lttng-tools

commit 5da88b0f58d7f838068037ea449ddfb25d3e85ad

Send ust and kernel domain directory handle to consumer
Send a directory handle to ust/ and kernel/ subdirectories of the
chunk to the consumer daemon, and alter the paths sent to consumer
so they do not include the domain subdir.

This will facilitate implementation of the clear command when no
rotation has been performed. The clear command will need to move the
ust/ and kernel/ directories to a "deleted" temporary directory while
the consumer is still actively using those directories. Making sure that
the consumer uses a handle on those subdirectories only ensures that the
consumer will not observe any transient state when those directories are
moved.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Change-Id: I2357ad702f810561c4d11405edc3ef204fc6c455
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng-sessiond/kernel.c
@@ -1289,6 +1289,7 @@ enum lttng_error_code kernel_snapshot_record(
 	struct lttng_ht_iter iter;
 	struct ltt_kernel_metadata *saved_metadata;
 	char *trace_path = NULL;
+	size_t consumer_path_offset = 0;
 
 	assert(ksess);
 	assert(ksess->consumer);
@@ -1315,7 +1316,7 @@ enum lttng_error_code kernel_snapshot_record(
 	}
 
 	trace_path = setup_channel_trace_path(ksess->consumer,
-			DEFAULT_KERNEL_TRACE_DIR);
+			DEFAULT_KERNEL_TRACE_DIR, &consumer_path_offset);
 	if (!trace_path) {
 		status = LTTNG_ERR_INVALID;
 		goto error;
@@ -1338,7 +1339,7 @@ enum lttng_error_code kernel_snapshot_record(
 		cds_list_for_each_entry(chan, &ksess->channel_list.head, list) {
 			status = consumer_snapshot_channel(socket, chan->key, output, 0,
 					ksess->uid, ksess->gid,
-					trace_path, wait,
+					&trace_path[consumer_path_offset], wait,
 					nb_packets_per_stream);
 			if (status != LTTNG_OK) {
 				(void) kernel_consumer_destroy_metadata(socket,
@@ -1349,7 +1350,8 @@ enum lttng_error_code kernel_snapshot_record(
 
 		/* Snapshot metadata, */
 		status = consumer_snapshot_channel(socket, ksess->metadata->key, output,
-				1, ksess->uid, ksess->gid, trace_path, wait, 0);
+				1, ksess->uid, ksess->gid, &trace_path[consumer_path_offset],
+				wait, 0);
 		if (status != LTTNG_OK) {
 			goto error_consumer;
 		}

src/bin/lttng-sessiond/ust-app.c
@@ -1901,12 +1901,12 @@ static void shadow_copy_session(struct ust_app_session *ua_sess,
 		switch (ua_sess->buffer_type) {
 		case LTTNG_BUFFER_PER_PID:
 			ret = snprintf(tmp_shm_path, sizeof(tmp_shm_path),
-					DEFAULT_UST_TRACE_PID_PATH "/%s-%d-%s",
+					"/" DEFAULT_UST_TRACE_PID_PATH "/%s-%d-%s",
 					app->name, app->pid, datetime);
 			break;
 		case LTTNG_BUFFER_PER_UID:
 			ret = snprintf(tmp_shm_path, sizeof(tmp_shm_path),
-					DEFAULT_UST_TRACE_UID_PATH,
+					"/" DEFAULT_UST_TRACE_UID_PATH,
 					app->uid, app->bits_per_long);
 			break;
 		default:
@@ -5921,6 +5921,7 @@ enum lttng_error_code ust_app_snapshot_record(
 			struct buffer_reg_channel *reg_chan;
 			struct consumer_socket *socket;
 			char pathname[PATH_MAX];
+			size_t consumer_path_offset = 0;
 
 			if (!reg->registry->reg.ust->metadata_key) {
 				/* Skip since no metadata is present */
@@ -5936,12 +5937,8 @@ enum lttng_error_code ust_app_snapshot_record(
 			}
 
 			memset(pathname, 0, sizeof(pathname));
-			/*
-			 * DEFAULT_UST_TRACE_UID_PATH already contains a path
-			 * separator.
-			 */
 			ret = snprintf(pathname, sizeof(pathname),
-					DEFAULT_UST_TRACE_DIR DEFAULT_UST_TRACE_UID_PATH,
+					DEFAULT_UST_TRACE_DIR "/" DEFAULT_UST_TRACE_UID_PATH,
 					reg->uid, reg->bits_per_long);
 			if (ret < 0) {
 				PERROR("snprintf snapshot path");
@@ -5950,7 +5947,8 @@ enum lttng_error_code ust_app_snapshot_record(
 			}
 			/* Free path allowed on previous iteration. */
 			free(trace_path);
-			trace_path = setup_channel_trace_path(usess->consumer, pathname);
+			trace_path = setup_channel_trace_path(usess->consumer, pathname,
+						&consumer_path_offset);
 			if (!trace_path) {
 				status = LTTNG_ERR_INVALID;
 				goto error;
@@ -5961,7 +5959,7 @@ enum lttng_error_code ust_app_snapshot_record(
 				status = consumer_snapshot_channel(socket,
 						reg_chan->consumer_key,
 						output, 0, usess->uid,
-						usess->gid, trace_path, wait,
+						usess->gid, &trace_path[consumer_path_offset], wait,
 						nb_packets_per_stream);
 				if (status != LTTNG_OK) {
 					goto error;
@@ -5969,7 +5967,8 @@ enum lttng_error_code ust_app_snapshot_record(
 			}
 			status = consumer_snapshot_channel(socket,
 					reg->registry->reg.ust->metadata_key, output, 1,
-					usess->uid, usess->gid, trace_path, wait, 0);
+					usess->uid, usess->gid, &trace_path[consumer_path_offset],
+					wait, 0);
 			if (status != LTTNG_OK) {
 				goto error;
 			}
@@ -5985,6 +5984,7 @@ enum lttng_error_code ust_app_snapshot_record(
 			struct ust_app_session *ua_sess;
 			struct ust_registry_session *registry;
 			char pathname[PATH_MAX];
+			size_t consumer_path_offset = 0;
 
 			ua_sess = lookup_session_by_app(usess, app);
 			if (!ua_sess) {
@@ -6002,7 +6002,7 @@ enum lttng_error_code ust_app_snapshot_record(
 
 			/* Add the UST default trace dir to path. */
 			memset(pathname, 0, sizeof(pathname));
-			ret = snprintf(pathname, sizeof(pathname), DEFAULT_UST_TRACE_DIR "%s",
+			ret = snprintf(pathname, sizeof(pathname), DEFAULT_UST_TRACE_DIR "/%s",
 					ua_sess->path);
 			if (ret < 0) {
 				status = LTTNG_ERR_INVALID;
@@ -6011,7 +6011,8 @@ enum lttng_error_code ust_app_snapshot_record(
 			}
 			/* Free path allowed on previous iteration. */
 			free(trace_path);
-			trace_path = setup_channel_trace_path(usess->consumer, pathname);
+			trace_path = setup_channel_trace_path(usess->consumer, pathname,
+					&consumer_path_offset);
 			if (!trace_path) {
 				status = LTTNG_ERR_INVALID;
 				goto error;
@@ -6024,7 +6025,7 @@ enum lttng_error_code ust_app_snapshot_record(
 								.uid,
 						ua_sess->effective_credentials
 								.gid,
-						trace_path, wait,
+						&trace_path[consumer_path_offset], wait,
 						nb_packets_per_stream);
 				switch (status) {
 				case LTTNG_OK:
@@ -6045,7 +6046,7 @@ enum lttng_error_code ust_app_snapshot_record(
 					registry->metadata_key, output, 1,
 					ua_sess->effective_credentials.uid,
 					ua_sess->effective_credentials.gid,
-					trace_path, wait, 0);
+					&trace_path[consumer_path_offset], wait, 0);
 			switch (status) {
 			case LTTNG_OK:
 				break;
@@ -6459,7 +6460,7 @@ enum lttng_error_code ust_app_create_channel_subdirectories(
 
 		cds_list_for_each_entry(reg, &usess->buffer_reg_uid_list, lnode) {
 			fmt_ret = asprintf(&pathname_index,
-				       DEFAULT_UST_TRACE_DIR DEFAULT_UST_TRACE_UID_PATH "/" DEFAULT_INDEX_DIR,
+				       DEFAULT_UST_TRACE_DIR "/" DEFAULT_UST_TRACE_UID_PATH "/" DEFAULT_INDEX_DIR,
 				       reg->uid, reg->bits_per_long);
 			if (fmt_ret < 0) {
 				ERR("Failed to format channel index directory");
@@ -6515,7 +6516,7 @@ enum lttng_error_code ust_app_create_channel_subdirectories(
 			}
 
 			fmt_ret = asprintf(&pathname_index,
-					DEFAULT_UST_TRACE_DIR "%s/" DEFAULT_INDEX_DIR,
+					DEFAULT_UST_TRACE_DIR "/%s/" DEFAULT_INDEX_DIR,
 					ua_sess->path);
 			if (fmt_ret < 0) {
 				ERR("Failed to format channel index directory");

commit 1eda9ed552c430754e7568493c0dccd697b25e9e

Fix: snapshot path have domain subdir duplicate "ust/ust" or "kernel/kernel"
Observed issue
==============

lttng-ivc observed unexpected path generated for streamed snapshot:

  joraj-alpa/
   test-20190319-120000-20210113-110101/
    snapshot-1-20210113-110102-0/
*     ust/
*      ust/
        pid/
         app-ust-2362198-20210113-110101/
           channel_0
           channel_1
           metadata
           channel_2
           channel_3

"ust" the domain subdir is present two-time instead off only one time.

The same problem is seen for kernel snapshot.

Cause
=====

Based on dissection the problem was introduced by commit
5da88b0f58d7f838068037ea449ddfb25d3e85ad [1]

For snapshots, the consumer output object of the snapshot output and
information is fetched from the *original* consumer output.
(code block around src/bin/lttng-sessiond/cmd.c:4748)

The snapshot consumer output does not contains the necessary information
to populate domain_subdir on copy (domain_subdir is '\0').

This would lead to a len evaluation of 1 for the consumer_path_offset in
setup_channel_trace_path:70. This would end up not "skipping" the "ust"
and "kernel" part of the path.

Solution
========

Part of the solution is to copy the domain_subdir from the original
session consumer output to the snapshot output.

Still, the problem was still present since that now that the
domain_subdir was not "\0", the value was suffixed to the passed
session_path. In the snapshot code path, "ust/" and "kernel/" were
already present in the session_path passed to setup_channel_trace_path.

A quick modification at the caller level in the snapshot code path fixes
the issues once and for all.

Tests
=======
The test suit is augmented for certain key tests to validate the complete path
of a trace.

Path validation is based on crude bash pattern matching.

Known drawbacks
=========
None

References
==========
[1] https://github.com/lttng/lttng-tools/commit/5da88b0f58d7f838068037ea449ddfb25d3e85ad

src/bin/lttng-sessiond/cmd.c
@@ -4751,6 +4751,11 @@ enum lttng_error_code snapshot_record(struct ltt_session *session,
 				consumer_copy_output(snapshot_output->consumer);
 		strcpy(snapshot_kernel_consumer_output->chunk_path,
 			snapshot_chunk_name);
+
+		/* Copy the original domain subdir */
+		strcpy(snapshot_kernel_consumer_output->domain_subdir,
+				original_kernel_consumer_output->domain_subdir);
+
 		ret = consumer_copy_sockets(snapshot_kernel_consumer_output,
 				original_kernel_consumer_output);
 		if (ret < 0) {
@@ -4773,6 +4778,11 @@ enum lttng_error_code snapshot_record(struct ltt_session *session,
 				consumer_copy_output(snapshot_output->consumer);
 		strcpy(snapshot_ust_consumer_output->chunk_path,
 			snapshot_chunk_name);
+
+		/* Copy the original domain subdir */
+		strcpy(snapshot_ust_consumer_output->domain_subdir,
+				original_ust_consumer_output->domain_subdir);
+
 		ret = consumer_copy_sockets(snapshot_ust_consumer_output,
 				original_ust_consumer_output);
 		if (ret < 0) {

src/bin/lttng-sessiond/kernel.c
@@ -1579,7 +1579,7 @@ enum lttng_error_code kernel_snapshot_record(
 	}
 
 	trace_path = setup_channel_trace_path(ksess->consumer,
-			DEFAULT_KERNEL_TRACE_DIR, &consumer_path_offset);
+			"", &consumer_path_offset);
 	if (!trace_path) {
 		status = LTTNG_ERR_INVALID;
 		goto error;

src/bin/lttng-sessiond/ust-app.c
@@ -5973,7 +5973,7 @@ enum lttng_error_code ust_app_snapshot_record(
 
 			memset(pathname, 0, sizeof(pathname));
 			ret = snprintf(pathname, sizeof(pathname),
-					DEFAULT_UST_TRACE_DIR "/" DEFAULT_UST_TRACE_UID_PATH,
+					DEFAULT_UST_TRACE_UID_PATH,
 					reg->uid, reg->bits_per_long);
 			if (ret < 0) {
 				PERROR("snprintf snapshot path");
@@ -6037,7 +6037,7 @@ enum lttng_error_code ust_app_snapshot_record(
 
 			/* Add the UST default trace dir to path. */
 			memset(pathname, 0, sizeof(pathname));
-			ret = snprintf(pathname, sizeof(pathname), DEFAULT_UST_TRACE_DIR "/%s",
+			ret = snprintf(pathname, sizeof(pathname), "%s",
 					ua_sess->path);
 			if (ret < 0) {
 				status = LTTNG_ERR_INVALID;

tests/regression/kernel/test_event_basic
@@ -9,7 +9,7 @@ TEST_DESC="Kernel tracer - Basic event"
 
 CURDIR=$(dirname $0)/
 TESTDIR=$CURDIR/../..
-NUM_TESTS=20
+NUM_TESTS=21
 
 TESTCMD="/bin/true"
 
@@ -33,6 +33,7 @@ function test_event_basic()
 	eval ${TESTCMD}
 	stop_lttng_tracing_ok
 
+	validate_trace_path_kernel "$TRACE_PATH" ""
 	validate_trace "sched_switch" $TRACE_PATH
 	validate_trace "sched_process_exit" $TRACE_PATH
 	validate_trace "sched_process_fork" $TRACE_PATH

tests/regression/tools/base-path/test_ust
@@ -40,6 +40,7 @@ function ust_app_stream_base_path ()
 	destroy_lttng_session_ok $session_name
 
 	# validate test
+	validate_trace_path_ust_uid_network "$TRACE_PATH" "" "$base_path"
 	if validate_trace $EVENT_NAME "$TRACE_PATH/$HOSTNAME/$base_path"; then
 		# only delete if successful
 		rm -rf "$TRACE_PATH"
@@ -67,6 +68,7 @@ function ust_app_snapshot_create_base_path ()
 	destroy_lttng_session_ok $session_name
 
 	# validate test
+	validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "" "snapshot-1" 0 "$base_path"
 	if validate_trace $EVENT_NAME "$TRACE_PATH/$HOSTNAME/$base_path"; then
 		# only delete if successful
 		rm -rf "$TRACE_PATH"
@@ -93,6 +95,7 @@ function ust_app_snapshot_base_path ()
 	destroy_lttng_session_ok $session_name
 
 	# validate test
+	validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "" "snapshot-1" 0 "$base_path"
 	if validate_trace $EVENT_NAME "$TRACE_PATH/$HOSTNAME/$base_path"; then
 		# only delete if successful
 		rm -rf "$TRACE_PATH"
@@ -120,6 +123,7 @@ function ust_app_snapshot_add_output_base_path ()
 	destroy_lttng_session_ok $session_name
 
 	# validate test
+	validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "" "snapshot-1" 0 "$base_path"
 	if validate_trace $EVENT_NAME "$TRACE_PATH/$HOSTNAME/$base_path"; then
 		# only delete if successful
 		rm -rf "$TRACE_PATH"
@@ -141,6 +145,7 @@ function ust_app_stream_base_path_via_load ()
 	destroy_lttng_session_ok $session_name
 
 	# validate test
+	validate_trace_path_ust_uid_network "$TRACE_PATH" "" "$base_path"
 	if validate_trace $EVENT_NAME "$TRACE_PATH/$HOSTNAME/$base_path"; then
 		# only delete if successful
 		rm -rf "$TRACE_PATH"

tests/regression/tools/live/test_lttng_kernel
@@ -18,7 +18,7 @@ TRACE_PATH=$(mktemp -d)
 
 DIR=$(readlink -f $TESTDIR)
 
-NUM_TESTS=10
+NUM_TESTS=11
 
 source $TESTDIR/utils/utils.sh
 
@@ -64,6 +64,7 @@ skip $isroot "Root access is needed. Skipping all tests." $NUM_TESTS ||
 
 	clean_live_tracing
 
+	validate_trace_path_kernel_network "$TRACE_PATH" "$SESSION_NAME"
 	validate_trace $EVENT_NAME $TRACE_PATH
 	rm -rf $TRACE_PATH
 

tests/regression/tools/snapshots/test_kernel
@@ -14,7 +14,7 @@ CHANNEL_NAME="snapchan"
 
 TRACE_PATH=$(mktemp -d)
 
-NUM_TESTS=2060
+NUM_TESTS=2061
 
 source $TESTDIR/utils/utils.sh
 
@@ -31,12 +31,11 @@ function test_kernel_local_snapshot ()
 	destroy_lttng_session_ok $SESSION_NAME
 
 	# Validate test
+	validate_trace_path_kernel_snapshot "$TRACE_PATH" "" "snapshot-1" 0 ""
 	validate_trace $EVENT_NAME $TRACE_PATH/
 	if [ $? -eq 0 ]; then
 		# Only delete if successful
 		rm -rf $TRACE_PATH
-	else
-		break
 	fi
 }
 

tests/regression/tools/snapshots/test_kernel_streaming
@@ -14,7 +14,7 @@ CHANNEL_NAME="chan1"
 
 TRACE_PATH=$(mktemp -d)
 
-NUM_TESTS=61
+NUM_TESTS=66
 
 source $TESTDIR/utils/utils.sh
 
@@ -127,6 +127,7 @@ function test_kernel_n_snapshot()
 		start_lttng_tracing_ok $SESSION_NAME
 		lttng_snapshot_record $SESSION_NAME
 		stop_lttng_tracing_ok $SESSION_NAME
+		validate_trace_path_kernel_snapshot_network "$TRACE_PATH" "$SESSION_NAME" "snapshot-1" $(( i - 1 )) ""
 		validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*/snapshot-1*
 		if [ $? -ne 0 ]; then
 			return 1

tests/regression/tools/snapshots/test_ust_streaming
@@ -21,7 +21,7 @@ APPS_PID=
 
 TRACE_PATH=$(mktemp -d)
 
-NUM_TESTS=75
+NUM_TESTS=85
 
 source $TESTDIR/utils/utils.sh
 
@@ -89,6 +89,7 @@ function test_ust_default_name_with_del()
 	lttng_snapshot_record $SESSION_NAME
 
 	# Validate test
+	validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "$SESSION_NAME" "snapshot-1" 0
 	validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*/snapshot-1*
 	if [ $? -ne 0 ]; then
 		stop_test_apps
@@ -100,6 +101,7 @@ function test_ust_default_name_with_del()
 	lttng_snapshot_record $SESSION_NAME
 
 	# Validate test with the next ID since a del output was done prior.
+	validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "$SESSION_NAME" "snapshot-2" 1
 	validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*/snapshot-2*
 	if [ $? -ne 0 ]; then
 		stop_test_apps
@@ -130,6 +132,7 @@ function test_ust_default_name()
 	stop_lttng_tracing_ok $SESSION_NAME
 	destroy_lttng_session_ok $SESSION_NAME
 	# Validate test
+	validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "$SESSION_NAME" "snapshot-1" 0
 	validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*/snapshot-1*
 	out=$?
 
@@ -153,6 +156,7 @@ function test_ust_default_name_custom_uri()
 	stop_lttng_tracing_ok $SESSION_NAME
 	destroy_lttng_session_ok $SESSION_NAME
 	# Validate test
+	validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "$SESSION_NAME" "snapshot-1" 0
 	validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*/snapshot-1*
 	out=$?
 
@@ -183,6 +187,7 @@ function test_ust_custom_name()
 	if ls $TRACE_PATH/$HOSTNAME/$SESSION_NAME*/$name* &> /dev/null; then
 		ok 0 "Custom name snapshot exists"
 		# Validate test
+		validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "$SESSION_NAME" "$name" 0
 		validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*/$name-*
 		out=$?
 	else
@@ -201,13 +206,14 @@ function test_ust_n_snapshot()
 	create_lttng_session_no_output $SESSION_NAME
 	enable_lttng_mmap_overwrite_ust_channel $SESSION_NAME $CHANNEL_NAME
 	enable_ust_lttng_event_ok $SESSION_NAME $EVENT_NAME $CHANNEL_NAME
-	snapshot_add_output $SESSION_NAME "net://localhost" $name
+	snapshot_add_output $SESSION_NAME "net://localhost"
 	start_test_app
-	for i in {1..5};
+	for i in {0..4};
 	do
 		start_lttng_tracing_ok $SESSION_NAME
 		lttng_snapshot_record $SESSION_NAME
 		stop_lttng_tracing_ok $SESSION_NAME
+		validate_trace_path_ust_uid_snapshot_network "$TRACE_PATH" "$SESSION_NAME" "snapshot-1" $i
 		validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*/snapshot-1*
 		if [ $? -ne 0 ]; then
 			return 1

tests/regression/tools/snapshots/ust_test
@@ -16,7 +16,7 @@ TESTAPP_NAME="gen-ust-events"
 TESTAPP_BIN="$TESTAPP_PATH/$TESTAPP_NAME/$TESTAPP_NAME"
 APPS_PID=
 
-NUM_TESTS=100
+NUM_TESTS=105
 
 TRACE_PATH=$(mktemp -d)
 
@@ -32,7 +32,7 @@ if [ -z $1 ]; then
 fi
 NR_SNAPSHOT=$1
 
-NUM_TESTS=$(($NUM_TESTS + ($NR_SNAPSHOT * 2)))
+NUM_TESTS=$(($NUM_TESTS + ($NR_SNAPSHOT * 3)))
 
 function start_test_app()
 {
@@ -193,6 +193,7 @@ function test_ust_local_snapshot ()
 	destroy_lttng_session_ok $SESSION_NAME
 
 	# Validate test
+	validate_trace_path_ust_uid_snapshot "$TRACE_PATH" "" "snapshot-1" 0
 	validate_trace $EVENT_NAME $TRACE_PATH/
 	if [ $? -eq 0 ]; then
 		# Only delete if successful
@@ -335,6 +336,7 @@ function test_ust_local_snapshot_max_size ()
 	destroy_lttng_session_ok $SESSION_NAME
 
 	# Validate test
+	validate_trace_path_ust_uid_snapshot "$TRACE_PATH" "" "snapshot-1" 0
 	validate_trace $EVENT_NAME $TRACE_PATH/
 
 	if [ $? -eq 0 ]; then
@@ -365,6 +367,7 @@ function test_ust_local_snapshot_large_metadata ()
 	destroy_lttng_session_ok $SESSION_NAME
 
 	# Validate test
+	validate_trace_path_ust_uid_snapshot "$TRACE_PATH" "" "snapshot-1" 0
 	validate_trace $LM_EVENT $TRACE_PATH/
 	if [ $? -eq 0 ]; then
 		# Only delete if successful
@@ -400,6 +403,7 @@ function test_ust_per_uid_local_snapshot ()
 	destroy_lttng_session_ok $SESSION_NAME
 
 	# Validate test
+	validate_trace_path_ust_uid_snapshot "$TRACE_PATH" "" "snapshot-1" 0
 	validate_trace $EVENT_NAME $TRACE_PATH/
 	if [ $? -eq 0 ]; then
 		# Only delete if successful
@@ -430,6 +434,7 @@ function test_ust_per_uid_local_snapshot_post_mortem ()
 	destroy_lttng_session_ok $SESSION_NAME
 
 	# Validate test
+	validate_trace_path_ust_uid_snapshot "$TRACE_PATH" "" "snapshot-1" 0
 	validate_trace $EVENT_NAME $TRACE_PATH/
 	if [ $? -eq 0 ]; then
 		# Only delete if successful
@@ -457,6 +462,7 @@ function test_ust_local_snapshots ()
 		rm -rf $TRACE_PATH/snapshot/* 2>/dev/null
 		lttng_snapshot_record $SESSION_NAME
 		# Validate test
+		validate_trace_path_ust_uid_snapshot "$TRACE_PATH" "" "snapshot-1" $((i - 1))
 		validate_trace $EVENT_NAME $TRACE_PATH/
 		if [ $? -eq 0 ]; then
 			# Only delete if successful

tests/regression/tools/streaming/test_kernel
@@ -13,7 +13,7 @@ SESSION_NAME=""
 
 TRACE_PATH=$(mktemp -d)
 
-NUM_TESTS=10
+NUM_TESTS=11
 
 source $TESTDIR/utils/utils.sh
 
@@ -68,6 +68,7 @@ skip $isroot "Root access is needed. Skipping all kernel streaming tests." $NUM_
 		${fct_test}
 
 		# Validate test
+		validate_trace_path_kernel_network "$TRACE_PATH" "$SESSION_NAME"
 		validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*
 		if [ $? -eq 0 ]; then
 			# Only delete if successful

tests/regression/tools/streaming/test_ust
@@ -18,7 +18,7 @@ EVENT_NAME="tp:tptest"
 
 TRACE_PATH=$(mktemp -d)
 
-NUM_TESTS=16
+NUM_TESTS=18
 
 source $TESTDIR/utils/utils.sh
 
@@ -91,6 +91,7 @@ do
 	${fct_test}
 
 	# Validate test
+	validate_trace_path_ust_uid_network "$TRACE_PATH" "$SESSION_NAME"
 	validate_trace $EVENT_NAME $TRACE_PATH/$HOSTNAME/$SESSION_NAME*
 	if [ $? -eq 0 ]; then
 		# Only delete if successful

tests/regression/tools/tracefile-limits/test_tracefile_size
@@ -17,7 +17,7 @@ TESTAPP_PATH="$TESTDIR/utils/testapp"
 TESTAPP_NAME="gen-ust-events"
 TESTAPP_BIN="$TESTAPP_PATH/$TESTAPP_NAME/$TESTAPP_NAME"
 
-NUM_TESTS=66
+NUM_TESTS=74
 
 source $TESTDIR/utils/utils.sh
 
@@ -115,6 +115,7 @@ function test_tracefile_size_limit ()
 
 	# Validate tracing data, we should at least have some events
 
+	validate_trace_path_ust_pid "$trace_path" "" "gen-ust-eve-ust"
 	validate_trace $event_name $trace_path
 
 	rm -rf $trace_path
@@ -153,6 +154,7 @@ function test_tracefile_size_limit_pagesize ()
 
 	# Validate tracing data, we should at least have some events
 
+	validate_trace_path_ust_pid "$trace_path" "" "gen-ust-eve-ust"
 	validate_trace $event_name $trace_path
 
 	rm -rf $trace_path

tests/utils/utils.sh
@@ -16,6 +16,11 @@ BABELTRACE_BIN="babeltrace"
 OUTPUT_DEST=/dev/null
 ERROR_OUTPUT_DEST=/dev/null
 
+# To match 20201127-175802
+date_time_pattern="[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]"
+# The size of a long on this system
+system_long_bit_size=$(getconf LONG_BIT)
+
 # Minimal kernel version supported for session daemon tests
 KERNEL_MAJOR_VERSION=2
 KERNEL_MINOR_VERSION=6
@@ -2103,4 +2108,279 @@ function lttng_clear_all ()
 {
 	$TESTDIR/../src/bin/lttng/$LTTNG_BIN clear --all 1> $OUTPUT_DEST 2> $ERROR_OUTPUT_DEST
 	ok $? "Clear all lttng sessions"
+}
+
+function validate_trace_path_kernel ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local pattern
+	local ret
+
+	# If the session was given a trace path on creation, there is no session
+	# name component to the path. Caller can simply not pass a session name
+	# for this scenario.
+	if [ -n "$session_name" ]; then
+		session_name="$session_name-$date_time_pattern"
+	fi
+
+	pattern="$tracepath/$session_name/kernel/metadata"
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "Base snapshot path is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
+}
+
+function validate_trace_path_kernel_network ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local hostname=$HOSTNAME
+	local pattern="$tracepath/$hostname/$session_name-$date_time_pattern/kernel/metadata"
+	local ret
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "Base snapshot path is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
+}
+function validate_trace_path_ust_uid ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local uid=$UID
+	local pattern="$tracepath/$session_name-$date_time_pattern/ust/uid/$uid/${system_long_bit_size}-bit/metadata"
+	local ret
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "Base snapshot path is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
+}
+
+function validate_trace_path_ust_uid_network ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local base_path=$3
+	local uid=$UID
+	local hostname=$HOSTNAME
+	local pattern
+	local ret
+
+	# If the session was given a network base path (e.g
+	# 127.0.0.1/my/custom/path on creation, there is no session name
+	# component to the path on the relayd side. Caller can simply not pass a
+	# session name for this scenario.
+	if [ -n "$session_name" ]; then
+		session_name="$session_name-$date_time_pattern"
+		if [ -n "$base_path" ]; then
+			fail "Session name and base path are mutually exclusive"
+			return 
+		fi
+	fi
+
+	pattern="$tracepath/$hostname/$base_path/$session_name/ust/uid/$uid/${system_long_bit_size}-bit/metadata"
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "UST uid network path pattern is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
+}
+function validate_trace_path_ust_pid ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local app_string=$3
+	local pid=$4
+	local pattern
+	local ret
+	
+	if [ -z "$tracepath" ]; then
+		fail "Missing base path"
+		return
+	fi
+
+	# If the session was given a trace path on creation, there is no session
+	# name component to the path. Caller can simply not pass a session name
+	# for this scenario.
+	if [ ! -z "$session_name" ]; then
+		session_name="$session_name-$date_time_pattern"
+	fi
+
+	pattern="$tracepath/$session_name/ust/pid/$pid/$app_string-*-$date_time_pattern/metadata"
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "Base snapshot path is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
+}
+
+function validate_trace_path_ust_uid_snapshot_network ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local snapshot_name=$3
+	local snapshot_number=$4
+	local base_path=$5
+	local hostname=$HOSTNAME
+	local uid=$UID
+	local pattern
+	local ret
+
+	# If the session/output was given a network base path (e.g
+	# 127.0.0.1/my/custom/path on creation, there is no session name
+	# component to the path on the relayd side. Caller can simply not pass a
+	# session name for this scenario.
+	if [ -n "$session_name" ]; then
+		session_name="$session_name-$date_time_pattern"
+		if [ -n "$base_path" ]; then
+			fail "Session name and base path are mutually exclusive"
+			return 
+		fi
+	fi
+
+	pattern="$tracepath/$hostname/$base_path/$session_name/$snapshot_name-$date_time_pattern-$snapshot_number/ust/uid/$uid/${system_long_bit_size}-bit/metadata"
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "Snapshot path is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
+}
+
+function validate_trace_path_ust_uid_snapshot ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local snapshot_name=$3
+	local snapshot_number=$4
+	local base_path=$5
+	local uid=$UID
+	local pattern
+	local ret
+
+	# If the session/output was given a network base path (e.g
+	# 127.0.0.1/my/custom/path on creation, there is no session name
+	# component to the path on the relayd side. Caller can simply not pass a
+	# session name for this scenario.
+	if [ -n "$session_name" ]; then
+		session_name="$session_name-$date_time_pattern"
+		if [ -n "$base_path" ]; then
+			fail "Session name and base path are mutually exclusive"
+			return 
+		fi
+	fi
+
+	pattern="$tracepath/$base_path/$session_name/$snapshot_name-$date_time_pattern-$snapshot_number/ust/uid/$uid/${system_long_bit_size}-bit/metadata"
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "Snapshot path is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
+}
+
+function validate_trace_path_kernel_snapshot_network ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local snapshot_name=$3
+	local snapshot_number=$4
+	local base_path=$5
+	local hostname=$HOSTNAME
+	local pattern
+	local ret
+
+	# If the session/output was given a network base path (e.g
+	# 127.0.0.1/my/custom/path on creation, there is no session name
+	# component to the path on the relayd side. Caller can simply not pass a
+	# session name for this scenario.
+	if [ -n "$session_name" ]; then
+		session_name="$session_name-$date_time_pattern"
+		if [ -n "$base_path" ]; then
+			fail "Session name and base path are mutually exclusive"
+			return 
+		fi
+	fi
+
+	pattern="$tracepath/$hostname/$base_path/$session_name/$snapshot_name-$date_time_pattern-$snapshot_number/kernel/metadata"
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "Snapshot path is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
+}
+function validate_trace_path_kernel_snapshot ()
+{
+	local tracepath=$1
+	local session_name=$2
+	local snapshot_name=$3
+	local snapshot_number=$4
+	local base_path=$5
+	local pattern
+	local ret
+
+	# If the session/output was given a network base path (e.g
+	# 127.0.0.1/my/custom/path on creation, there is no session name
+	# component to the path on the relayd side. Caller can simply not pass a
+	# session name for this scenario.
+	if [ -n "$session_name" ]; then
+		session_name="$session_name-$date_time_pattern"
+		if [ -n "$base_path" ]; then
+			fail "Session name and base path are mutually exclusive"
+			return 
+		fi
+	fi
+
+	pattern="$tracepath/$base_path/$session_name/$snapshot_name-$date_time_pattern-$snapshot_number/kernel/metadata"
+
+	[ -f $pattern ]
+	ret=$?
+	ok $ret "Snapshot path is valid"
+	
+	# joraj to remove
+	if [ "$ret" -ne "0" ]; then
+		echo "$pattern"
+		tree $tracepath
+	fi
 }