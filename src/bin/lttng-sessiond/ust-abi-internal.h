8bf084bbd620af2e27c915cca9a58b876939b2d0
Francis Deslauriers <francis.deslauriers@efficios.com>
2021-01-28 11:20:36 -0500

    ust-abi-internal.h: add counter event structs
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Change-Id: I6604e410401c4fb4fc4adfbea0065797dde77856

--- src/bin/lttng-sessiond/ust-abi-internal.h
+++ src/bin/lttng-sessiond/ust-abi-internal.h
@@ -117,6 +117,93 @@ struct lttng_ust_event_notifier_notification {
 	char padding[LTTNG_UST_EVENT_NOTIFIER_NOTIFICATION_PADDING];
 } LTTNG_PACKED;
 
+enum lttng_ust_key_token_type {
+	LTTNG_UST_KEY_TOKEN_STRING = 0,		/* arg: strtab_offset. */
+	LTTNG_UST_KEY_TOKEN_EVENT_NAME = 1,	/* no arg. */
+};
+
+#define LTTNG_UST_KEY_ARG_PADDING1		256
+#define LTTNG_UST_KEY_TOKEN_STRING_LEN_MAX	256
+struct lttng_ust_key_token {
+	uint32_t type;	/* enum lttng_ust_key_token_type */
+	union {
+		char string[LTTNG_UST_KEY_TOKEN_STRING_LEN_MAX];
+		char padding[LTTNG_UST_KEY_ARG_PADDING1];
+	} arg;
+} LTTNG_PACKED;
+
+#define LTTNG_UST_NR_KEY_TOKEN 4
+struct lttng_ust_counter_key_dimension {
+	uint32_t nr_key_tokens;
+	struct lttng_ust_key_token key_tokens[LTTNG_UST_NR_KEY_TOKEN];
+} LTTNG_PACKED;
+
+#define LTTNG_UST_COUNTER_DIMENSION_MAX 4
+struct lttng_ust_counter_key {
+	uint32_t nr_dimensions;
+	struct lttng_ust_counter_key_dimension key_dimensions[LTTNG_UST_COUNTER_DIMENSION_MAX];
+} LTTNG_PACKED;
+
+#define LTTNG_UST_COUNTER_EVENT_PADDING1	16
+struct lttng_ust_counter_event {
+	struct lttng_ust_event event;
+	struct lttng_ust_counter_key key;
+	char padding[LTTNG_UST_COUNTER_EVENT_PADDING1];
+} LTTNG_PACKED;
+
+enum lttng_ust_counter_arithmetic {
+	LTTNG_UST_COUNTER_ARITHMETIC_MODULAR = 0,
+	LTTNG_UST_COUNTER_ARITHMETIC_SATURATION = 1,
+};
+
+enum lttng_ust_counter_bitness {
+	LTTNG_UST_COUNTER_BITNESS_32 = 0,
+	LTTNG_UST_COUNTER_BITNESS_64 = 1,
+};
+
+struct lttng_ust_counter_dimension {
+	uint64_t size;
+	uint64_t underflow_index;
+	uint64_t overflow_index;
+	uint8_t has_underflow;
+	uint8_t has_overflow;
+} LTTNG_PACKED;
+
+struct lttng_ust_counter_conf {
+	uint32_t arithmetic;	/* enum lttng_ust_counter_arithmetic */
+	uint32_t bitness;	/* enum lttng_ust_counter_bitness */
+	uint32_t number_dimensions;
+	int64_t global_sum_step;
+	struct lttng_ust_counter_dimension dimensions[LTTNG_UST_COUNTER_DIMENSION_MAX];
+} LTTNG_PACKED;
+
+struct lttng_ust_counter_value {
+	uint32_t number_dimensions;
+	uint64_t dimension_indexes[LTTNG_UST_COUNTER_DIMENSION_MAX];
+	int64_t value;
+} LTTNG_PACKED;
+
+#define LTTNG_UST_COUNTER_PADDING1	(LTTNG_UST_SYM_NAME_LEN + 32)
+#define LTTNG_UST_COUNTER_DATA_MAX_LEN	4096U
+struct lttng_ust_counter {
+	uint64_t len;
+	char padding[LTTNG_UST_COUNTER_PADDING1];
+	char data[];	/* variable sized data */
+} LTTNG_PACKED;
+
+#define LTTNG_UST_COUNTER_GLOBAL_PADDING1	(LTTNG_UST_SYM_NAME_LEN + 32)
+struct lttng_ust_counter_global {
+	uint64_t len;		/* shm len */
+	char padding[LTTNG_UST_COUNTER_GLOBAL_PADDING1];
+} LTTNG_PACKED;
+
+#define LTTNG_UST_COUNTER_CPU_PADDING1	(LTTNG_UST_SYM_NAME_LEN + 32)
+struct lttng_ust_counter_cpu {
+	uint64_t len;		/* shm len */
+	uint32_t cpu_nr;
+	char padding[LTTNG_UST_COUNTER_CPU_PADDING1];
+} LTTNG_PACKED;
+
 enum lttng_ust_field_type {
 	LTTNG_UST_FIELD_OTHER			= 0,
 	LTTNG_UST_FIELD_INTEGER			= 1,
